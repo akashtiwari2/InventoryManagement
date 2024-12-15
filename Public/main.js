59f49d51efe4692cdc490789d8085947e97f7dea
akash <akashtiwariiet@gmail.com>
2024-12-16 00:08:54 +0530

    v0.0.2

--- Public/main.js
+++ Public/main.js
@@ -1,6 +1,6 @@
 function deleteProduct(id)
 {console.log(id)
-  const choice = confirm("Are you sure fir add bi krna adega soochlo");
+  const choice = confirm("Are you sure....... fir add bi krna padega soochlo");
   if (choice) {
     fetch("/delete_product/"+id, { method: "POST" }).then((res) => {
       if (res.ok) {
